68044734f88c95f314adfa1675f4fd0446195b30
toffet-masajo <kcmasajo@yahoo.com>
2023-02-22 02:00:59 +0800

    Exercise for Part1.8

--- part1b/fso-kcmasajo/src/App.js
+++ part1b/fso-kcmasajo/src/App.js
@@ -5,21 +5,30 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const getTotal = () => (good + neutral + bad);
+  const Statistics = (props) => {
+    const {good, neutral, bad} = props;
+    const total = good + neutral + bad;
+
+    return (
+      <div>
+        <h2>statistics</h2>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {total}</div>
+        <div>average {(good-bad)/total}</div>
+        <div>positive {(good*100)/total} %</div>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={() => { setGood(good + 1)}}>good</button>
-      <button onClick={() => { setNeutral(neutral + 1)}}>neutral</button>
-      <button onClick={() => { setBad(bad + 1)}}>bad</button>
-      <h2>statistics</h2>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {getTotal()}</div>
-      {getTotal() > 0 && <div>average {(good-bad)/getTotal()}</div>}
-      {getTotal() > 0 && <div>positive {((good*100)/getTotal())} %</div>}
+      <button onClick={() => { setGood(good + 1) }}>good</button>
+      <button onClick={() => { setNeutral(neutral + 1) }}>neutral</button>
+      <button onClick={() => { setBad(bad + 1) }}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   );
 }
