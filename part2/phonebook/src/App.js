5f60d2c7118b363f8d4d6ab1ee194602dc84bef2
toffet-masajo <kcmasajo@yahoo.com>
2023-03-01 03:03:04 +0800

    2.9* Phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,27 +2,35 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-555-0001' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleNameChange = (event) => setNewName(event.target.value);
-
   const handleNumberChange = (event) => setNewNumber(event.target.value);
-
+  const handleFilterChange = (event) => setFilter(event.target.value);
+  
   const handleAdd = (event) => {
     event.preventDefault();
 
     const duplicate = persons.filter((person) => person.name === newName);
     if(duplicate.length > 0) alert(`${newName} is already added in the phonebook`);
-    else setPersons(persons.concat({ name: newName, number: newNumber }));
+    else setPersons(persons.concat({ name: newName, number: newNumber, id: persons.length+1 }));
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleAdd}>
+        <div>
+          filter shown with: <input value={filter} onChange={handleFilterChange} />
+        </div>
+      <h2>add a new</h2>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
@@ -34,7 +42,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {persons
+        .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+        .map(person => <div key={person.id}>{person.name} {person.number}</div>)}
     </div>
   )
 }
