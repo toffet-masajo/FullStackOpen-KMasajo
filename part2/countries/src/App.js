43f314e905f11a87bf21a3c24c6259bacb6b540a
toffet-masajo <kcmasajo@yahoo.com>
2023-03-14 03:59:05 +0800

    2.20* Data for countries step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,55 +1,35 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const URL = 'https://restcountries.com/v3.1/all';
+import CountriesList from './CountriesList';
+import Filter from './Filter';
+
+const COUNTRIES_URL = 'https://restcountries.com/v3.1/all';
 
 const App = () => {
-  const [filter, setFilter] = useState('');
+  const [searchString, setSearchString] = useState('');
   const [countries, setCountries] = useState([]);
-
+  
   useEffect(() => {
-    axios.get(URL)
+    axios.get(COUNTRIES_URL)
       .then(res => setCountries(res.data))
       .catch(error => console.log(error));
   }, []);
-
-  const handlerInputChange = (event => setFilter(event.target.value));
-
-  const showContent = () => {
-    const list = countries
-      .filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()));
-      
-    if(list.length === 1) {
-      const country = list[0];
-      return(
-        <div key={country.cca2}>
-          <h2>{country.name.common}</h2>
-          <>
-            capital {country.capital.reduce((capitalList, cap) => capitalList.concat(`${cap} `), '')} <br/>
-            area {country.area}
-          </>
-          <h3>languages:</h3>
-          <ul>{Object.values(country.languages).map(lang => <li key={lang}>{lang}</li>)}</ul>
-          <img src={`${country.flags['png']}`} alt={`${country.flags['alt']}`} />
-        </div>
-      );
-    } else if(list.length <= 10) {
-      return list.map(item =>
-        <div key={item.cca2}>
-          {item.name.common}
-          <button onClick={() => setFilter(item.name.common)}>show</button>
-        </div>
-      );
-    }
-    return 'Too many matches, specify another filter'
-  }
+  
+  const handlerInputChange = (event => setSearchString(event.target.value));
+  const handlerShowButton = (value => setSearchString(value));
 
   return(
     <div>
-      <div className="countries-form">find countries <input value={filter} onChange={handlerInputChange} /></div>
-      <div className="countries-content"> 
-        {filter.length > 0 && showContent()}
-      </div>
+      <Filter value={searchString} handler={handlerInputChange} />
+      
+      { searchString.length > 0 && 
+        <CountriesList 
+          data={countries} 
+          searchFilter={searchString} 
+          handler={handlerShowButton} 
+        />
+      }
     </div>  
   );
 };
